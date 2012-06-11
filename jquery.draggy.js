679f693abd654a3617610606e321ba00bdf8d726
umarashfaq <umar.mughal2@gmail.com>
2012-06-11 21:44:17 +0500

    fixed some major bugs

--- jquery.draggy.js
+++ jquery.draggy.js
@@ -32,7 +32,8 @@
 			
 			this.options.z_index = 999;
 			
-			// this.options.scrollbar_container_color = '#F2F2F2';
+			this.options.scrollbar_container_color = 'transparent';
+			this.options.scrollbar_container_border = '1px solid transparent';
 			this.options.scrollbar_container_hover_color = '#F2F2F2';
 			this.options.scrollbar_container_hover_border = '1px solid #D9D9D9';
 			
@@ -137,15 +138,16 @@
 				parent_outer_height = jq_parent.outerHeight(true);
 				
 				jq_scroll_container_y = $('<div/>')
-					.css({
-						'position' : 'relative',
-						'background' : this.options.scrollbar_container_hover_color,
-						'border': this.options.scrollbar_container_hover_border,
-						'width' : scroll_width,
-						'z-index' : this.options.z_index,
-						'float' : 'right'
-					})
+					.addClass('__draggy')
+					.addClass('__vertical')
+					.addClass('__state_1')
 					.height(parent_height - 2) // 2 pixels will be covered by borders
+					.mouseover(function(e){
+						return _this.mouseoverScrollYContainer(e);
+					})
+					.mouseout(function(e){
+						return _this.mouseoutScrollYContainer(e);
+					})
 					.insertAfter(jq_parent);
 				jq_clear = $('<br/>')
 					.css({
@@ -168,6 +170,7 @@
 				*/
 				
 				jq_scroll_y = $('<div/>')
+					/*
 					.css({
 						'position' : 'absolute',
 						'background' : this.options.scrollbar_color,
@@ -179,16 +182,15 @@
 						'top' : -1,
 						'left': -1
 					})
-					.hover(function(e){
-						_this.mouseoverScrollY(this, e);
-						return false;
-					}, function(e){
-						_this.mouseoutScrollY(this, e);
-						return false;
+					*/
+					.mouseover(function(e){
+						return _this.mouseoverScrollY(e);
+					})
+					.mouseout(function(e){
+						return _this.mouseoutScrollY(e);
 					})				
 					.mousedown(function(e){
-						_this.mousedownScrollY(this, e);
-						return false;
+						return _this.mousedownScrollY(e);
 					})	
 					.height(scroll_y_height)
 					.appendTo(jq_scroll_container_y);
@@ -205,22 +207,30 @@
 			
 			if (is_scroll_x_applicable) {
 				jq_scroll_container_x = $('<div/>')
+					/*
 					.css({
 						'position' : 'relative',
-						'background' : this.options.scrollbar_container_hover_color,
-						'border': this.options.scrollbar_container_hover_border,
+						'background' : this.options.scrollbar_container_color,
+						'border': this.options.scrollbar_container_border,
 						'height' : scroll_width,
 						'z-index' : this.options.z_index
 					})
+					*/
+					.addClass('__draggy')
+					.addClass('__horizontal')
+					.addClass('__state_1')
 //					.offset({
 //						'top' : parent_offset.top + jq_parent.height() - parent_diff_height - scroll_width,
 //						'left': parent_offset.left + parent_diff_width
 //					})
 					.width(jq_parent.width())
-					.hover(function(e){
-						_this.mouseoverScrollContainer(this, e);
-					}, function(e){
-						_this.mouseoutScrollContainer(this, e);
+					.mouseover(function(e){
+						return _this.mouseoverScrollContainer(e, true);
+						// return false;
+					})
+					.mouseout(function(e){
+						return _this.mouseoutScrollContainer(e);
+						// return false;
 					})
 					.insertAfter(jq_parent);
 				container_x_offset = jq_scroll_container_x.offset();
@@ -228,6 +238,7 @@
 				container_x_outer_width = jq_scroll_container_x.outerWidth(true);
 				scroll_x_width = ( target_width > parent_width ? parent_width / target_width : 1 ) * container_x_width;
 				jq_scroll_x = $('<div/>')
+					/*
 					.css({
 						'position' : 'absolute',
 						'background' : this.options.scrollbar_color,
@@ -239,17 +250,19 @@
 						'top' : -1,
 						'left': -1
 					})
+					*/
 					.width(scroll_x_width)
-					.hover(function(e){
-						_this.mouseoverScroll(this, e);
-						return false;
-					}, function(e){
-						_this.mouseoutScroll(this, e);
-						return false;
+					.mouseover(function(e){
+						return _this.mouseoverScroll(e);
+						// return false;
+					})
+					.mouseout(function(e){
+						return _this.mouseoutScroll(e);
+						// return false;
 					})				
 					.mousedown(function(e){
-						_this.mousedownScroll(this, e);
-						return false;
+						return _this.mousedownScroll(e);
+						// return false;
 					})		
 					.appendTo(jq_scroll_container_x);
 				scroll_x_outer_width = jq_scroll_x.outerWidth(true);
@@ -331,50 +344,70 @@
 			}
 		};
 		
-		Draggy.prototype.mouseoverScrollContainer = function(o, e) {
-			var jq_o = $(o),
-				jq_scroll = jq_o.children()
-					.css({
-						'background' : '#C0C0C0',
-						'border' : '1px solid #9A9A9A'
-					});
+		Draggy.prototype.getStateClasses = function(index, css) {
+			return (css.match (/\b__state_\d{1}/g) || []).join(' ');
 		};
 		
-		Draggy.prototype.mouseoutScrollContainer = function(o, e) {
-			var jq_o = $(o),
-				jq_scroll = jq_o.children()
-					.css({
-						'background' : this.options.scrollbar_color,
-						'border' : this.options.scrollbar_border
-					});
+		Draggy.prototype.setState = function(scroll, state) {
+			state = state ? state : 1;
+			
+			var cls = '__state_'+state,
+				jq_scroll_container = scroll === 'y' ? this.jq_scroll_container_y : this.jq_scroll_container_x;
+			
+			jq_scroll_container
+				.removeClass(this.getStateClasses)
+				.addClass(cls);
 		};
 		
-		Draggy.prototype.mouseoverScroll = function(o, e) {
-			var jq_o = $(o)
-				.css({
-					'background' : this.options.scrollbar_hover_color,
-					'border' : this.options.scrollbar_hover_border
-				});
-				
-			this.is_mouse_over_scroll_x = true;
+		Draggy.prototype.mouseoverScrollContainer = function(e, is_mouse_over_scroll_x_container) {
+			console.log('[mouseoverScrollContainer] mouseover scroll_container_x detected.');
+			this.setState('x', 2);			
+			this.is_mouse_over_scroll_x_container = true /* is_mouse_over_scroll_x_container */;
+			return false;
+			// console.log('[mouseoverScrollContainer] this.is_mouse_over_scroll_x_container: '+this.is_mouse_over_scroll_x_container);
 		};
 		
-		Draggy.prototype.mouseoutScroll = function(o, e) {
+		Draggy.prototype.mouseoutScrollContainer = function(e) {
+			// console.log('mouseoutScrollContainer() invoked ... ');
+			console.log('[mouseoutScrollContainer] mouseout scroll_container_x detected.');
+			this.setState('x');
+			this.is_mouse_over_scroll_x_container = false;
+			return false;
+		};
+		
+		Draggy.prototype.mouseoverScroll = function(e) {
+			console.log('[mouseoutScroll] mouseover scroll_x detected.');
+			
 			if (!this.is_mouse_down_on_scroll_x) {
-				var jq_o = $(o)
-				.css({
-					'background' : this.options.scrollbar_color,
-					'border' : this.options.scrollbar_border
-				});
+				this.setState('x', 3);
+			}
+			this.is_mouse_over_scroll_x = true;
+			// this.mouseoverScrollContainer(e);
+			return false;
+		};
+		
+		Draggy.prototype.mouseoutScroll = function(e) {
+			console.log('[mouseoutScroll] mouseout scroll_x detected.');			
+			if (!this.is_mouse_down_on_scroll_x /* && !this.is_mouse_over_scroll_x_container */ ) {
+				this.setState('x');
+				/*
+				if (!this.is_mouse_over_scroll_x_container) {
+					this.mouseoutScrollContainer(e);
+				}
+				*/
 			}
 			
 			this.is_mouse_over_scroll_x = false;
+			return false;
 		};
 		
-		Draggy.prototype.mousedownScroll = function(o, e) {
-			var jq_o = $(o),
+		Draggy.prototype.mousedownScroll = function(e) {
+			var jq_c = this.jq_scroll_container_x,
+				jq_o = this.jq_scroll_x,
 				left = jq_o.position().left + 2,
 				css_left = jq_o.css('left');
+			
+			this.setState('x', 4);
 			
 			css_left = css_left.substring(0, css_left.indexOf('px'));
 			
@@ -383,21 +416,21 @@
 			this.is_mouse_down_on_scroll_x = true;
 			this.scroll_left = left;
 			this.mouse_down_point = {x:e.pageX,y:e.pageY};
+			return false;
 		};
 		
-		Draggy.prototype.mouseupScroll = function(o, e) {			
-			if (!this.is_mouse_over_scroll_x) {
-				var jq_o = $(o)
-					.css({
-						'background' : this.options.scrollbar_color,
-						'border' : this.options.scrollbar_border
-					});
+		Draggy.prototype.mouseupScroll = function(e) {			
+			if (this.is_mouse_over_scroll_x) {
+				this.setState('x', 3);
+			} else {
+				this.setState('x');
 			}
+			
 			this.is_mouse_down_on_scroll_x = false;
 		};
 		
-		Draggy.prototype.mousemoveScroll = function(o, e) {
-			var jq_o = $(o),
+		Draggy.prototype.mousemoveScroll = function(e) {
+			var jq_o = this.jq_scroll_x,
 				jq_parent = this.jq_parent,
 				original_left = this.scroll_left,
 				op = this.mouse_down_point,
@@ -426,19 +459,31 @@
 			jq_parent.scrollLeft(left);
 		};
 		
-		Draggy.prototype.mouseoverScrollY = function(o, e) {
-			var jq_o = $(o)
+		Draggy.prototype.mouseoverScrollY = function(e) {
+			/*
+			var jq_o = this.jq_scroll_y
 				.css({
 					'background' : this.options.scrollbar_hover_color,
 					'border' : this.options.scrollbar_hover_border
 				});
 				
 			this.is_mouse_over_scroll_y = true;
+			*/
+			
+			// console.log('[mouseoutScroll] mouseover scroll_x detected.');
+			
+			if (!this.is_mouse_down_on_scroll_y) {
+				this.setState('y', 3);
+			}
+			this.is_mouse_over_scroll_y = true;
+			// this.mouseoverScrollContainer(e);
+			return false;
 		};
 		
-		Draggy.prototype.mouseoutScrollY = function(o, e) {
+		Draggy.prototype.mouseoutScrollY = function(e) {
+			/*
 			if (!this.is_mouse_down_on_scroll_y) {
-				var jq_o = $(o)
+				var jq_o = this.jq_scroll_y
 				.css({
 					'background' : this.options.scrollbar_color,
 					'border' : this.options.scrollbar_border
@@ -446,30 +491,55 @@
 			}
 			
 			this.is_mouse_over_scroll_y = false;
+			*/
+			
+			if (!this.is_mouse_down_on_scroll_y /* && !this.is_mouse_over_scroll_x_container */ ) {
+				this.setState('y');
+			}
+			
+			this.is_mouse_over_scroll_y = false;
+			return false;
 		};
 		
-		Draggy.prototype.mousedownScrollY = function(o, e) {
-			var jq_o = $(o),
+		Draggy.prototype.mousedownScrollY = function(e) {
+			var jq_o = this.jq_scroll_y,
 				top = jq_o.position().top + 2;
 			
+			this.setState('y', 4);
+			
 			this.is_mouse_down_on_scroll_y = true;
-			this.scroll_top = top;
+			this.scroll_top = top;			
 			this.mouse_down_point = {x:e.pageX,y:e.pageY};
+			// console.log('[mousedownScrollY] scroll_top: '+top);
+			return false;
 		};
 		
-		Draggy.prototype.mouseupScrollY = function(o, e) {			
+		Draggy.prototype.mouseupScrollY = function(e) {		
+			/*
+			var jq_o = this.jq_scroll_y
+				.css({
+					'box-shadow' : ''
+				});
 			if (!this.is_mouse_over_scroll_y) {
-				var jq_o = $(o)
-					.css({
+				jq_o.css({
 						'background' : this.options.scrollbar_color,
 						'border' : this.options.scrollbar_border
 					});
 			}
 			this.is_mouse_down_on_scroll_y = false;
+			*/
+			
+			if (this.is_mouse_over_scroll_y) {
+				this.setState('y', 3);
+			} else {
+				this.setState('y');
+			}
+			
+			this.is_mouse_down_on_scroll_y = false;
 		};
 		
-		Draggy.prototype.mousemoveScrollY = function(o, e) {
-			var jq_o = $(o),
+		Draggy.prototype.mousemoveScrollY = function(e) {
+			var jq_o = this.jq_scroll_y,
 				jq_parent = this.jq_parent,
 				original_top = this.scroll_top,
 				op = this.mouse_down_point,
@@ -478,40 +548,88 @@
 				displacement_factor_y = this.displacement_factor_y,
 				top = ( original_top - distance.y ) / displacement_factor_y;
 			
+			console.log('[mousemoveScrollY] ( original_top('+original_top+') - distance.y('+distance.y+') ) / displacement_factor_y ('+displacement_factor_y+') = '+top);
+			
 			// console.log('[mousemoveScrollY] setting top: '+top);
 			
 			jq_parent.scrollTop(top);
 		};
 		
+		Draggy.prototype.mouseoverScrollYContainer = function(e) {
+			/*
+			var jq_o = this.jq_scroll_container_y
+				.css({
+					'background' : this.options.scrollbar_container_hover_color,
+					'border' : this.options.scrollbar_container_hover_border
+				}),
+				jq_scroll = jq_o.children()
+					.css({
+						'background' : '#C0C0C0',
+						'border' : '1px solid #9A9A9A'
+					});
+			*/
+			
+			this.setState('y', 2);			
+			this.is_mouse_over_scroll_y_container = true;
+			return false;
+		};
+		
+		Draggy.prototype.mouseoutScrollYContainer = function(e) {
+			/*
+			var jq_o = this.jq_scroll_container_y
+				.css({
+					'background' : this.options.scrollbar_container_color,
+					'border' : this.options.scrollbar_container_border
+				}),
+				jq_scroll = jq_o.children()
+					.css({
+						'background' : this.options.scrollbar_color,
+						'border' : this.options.scrollbar_border
+					});
+			*/
+			
+			this.setState('y');
+			this.is_mouse_over_scroll_y_container = false;
+			return false;
+		};
+		
 		Draggy.prototype.mousemoveDoc = function(e) {
 			var is_mouse_down_on_scroll_x = this.is_mouse_down_on_scroll_x,
 				is_mouse_down_on_scroll_y = this.is_mouse_down_on_scroll_y,
-				jq_scroll_x = this.jq_scroll_x,
-				jq_scroll_y = this.jq_scroll_y,
 				is_mouse_down = this.is_mouse_down;
 			
 			if (is_mouse_down) {
 				this.mousemove(e);
 			} else if (is_mouse_down_on_scroll_x) {
-				this.mousemoveScroll(jq_scroll_x, e);
+				this.mousemoveScroll(e);
 			} else if (is_mouse_down_on_scroll_y) {
-				this.mousemoveScrollY(jq_scroll_y, e);
+				this.mousemoveScrollY(e);
 			}			
 		};
 		
 		Draggy.prototype.mouseupDoc = function(e) {
 			var is_mouse_down_on_scroll_x = this.is_mouse_down_on_scroll_x,
+				is_mouse_over_scroll_x = this.is_mouse_over_scroll_x,
+				is_mouse_over_scroll_x_container = this.is_mouse_over_scroll_x_container,
 				is_mouse_down_on_scroll_y = this.is_mouse_down_on_scroll_y,
-				jq_scroll_x = this.jq_scroll_x,
-				jq_scroll_y = this.jq_scroll_y,
+				is_mouse_over_scroll_y = this.is_mouse_over_scroll_y,
+				is_mouse_over_scroll_y_container = this.is_mouse_over_scroll_x_container,
 				is_mouse_down = this.is_mouse_down;
 			
+			console.log('is_mouse_over_scroll_x_container: '+is_mouse_over_scroll_x_container);
+			
 			if (is_mouse_down_on_scroll_x) {
-				this.mouseupScroll(jq_scroll_x, e);
+				this.mouseupScroll(e);				
+			}	
+			
+			/*
+			if (!is_mouse_over_scroll_x && !is_mouse_over_scroll_x_container) {
+				this.mouseoutScrollContainer(e);
 			}
+			*/
 			
 			if (is_mouse_down_on_scroll_y) {
-				this.mouseupScrollY(jq_scroll_y, e);
+				this.mouseupScrollY(e);
 			}
 			
 			if (is_mouse_down) {
